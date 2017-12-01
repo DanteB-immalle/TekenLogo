7086d1ec3b361345275ff592227c86ed173ad420
Dante Budin <dante.budin@immalle.eu>
2017-12-01 11:01:19 +0100

    TekenLogo

--- TekenLogo/MainWindow.xaml.cs
+++ TekenLogo/MainWindow.xaml.cs
@@ -24,5 +24,49 @@ namespace TekenLogo
         {
             InitializeComponent();
         }
-    }
+
+        private void MijnKnop_Click(object sender, RoutedEventArgs e)
+        {
+            SolidColorBrush brush = new SolidColorBrush(Colors.Red);
+            DrawLogo(canvas, brush, 10, 20);
+            DrawLogo(canvas, brush, 100, 100);
+         
+            DrawTriangle(canvas, brush, 100, 10, 40, 40);
+            DrawTriangle(canvas, brush, 10, 100, 20, 60);
+            
+
+
+        }
+        private void DrawRectangle(Canvas drawingArea, SolidColorBrush brushToUse, double xPos, double yPos, double size)
+        {
+            Rectangle rect = new Rectangle();
+            rect.Width = size;
+            rect.Height = size;
+            rect.Margin = new Thickness(xPos, yPos, 0, 0);
+            rect.Stroke = brushToUse;
+            canvas.Children.Add(rect);
+     
+        }
+        private void DrawLogo(Canvas drawingArea, SolidColorBrush brushToUse, double xPos, double yPos)
+        {
+            DrawRectangle(drawingArea, brushToUse, xPos, yPos, 20);
+            DrawRectangle(drawingArea, brushToUse, xPos, yPos, 40);
+            DrawRectangle(drawingArea, brushToUse, xPos, yPos, 60);
+        }
+        private void DrawTriangle(Canvas drawingArea, SolidColorBrush brushToUse, double xPlace, double yPlace, double width, double height)
+        {
+            DrawLine(drawingArea, brushToUse, xPlace, yPlace, xPlace, yPlace + height);
+            DrawLine(drawingArea, brushToUse, xPlace, yPlace + height, xPlace + width, yPlace + height);
+            DrawLine(drawingArea, brushToUse, xPlace, yPlace, xPlace + width, yPlace + height);
+        }
+        private void DrawLine(Canvas drawingArea, SolidColorBrush brushToUse, double startX, double startY, double endX, double endY)
+        {
+            Line l = new Line();
+            l.X1 = startX;  l.X2 = endX;
+            l.Y1 = startY;  l.Y2 = endY;
+            l.Stroke = brushToUse;
+            drawingArea.Children.Add(l);
+        }
+
+        }
 }
